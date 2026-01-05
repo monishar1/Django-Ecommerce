ffa924cf4484ed60d0a42827c725f89f8e7fd407
monishar1 <moniradha1234@gmail.com>
2026-01-05 12:24:04 +0530

    commit

--- Eshop/mainapp/views.py
+++ Eshop/mainapp/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def homeview(request):
+    template = 'mainapp/home.html'
+    context = {}
+
+    return render(request, template_name= template, context= context)
+
+def aboutview(request):
+    template = 'mainapp/about.html'
+    context = {}
+
+    return render(request, template_name= template, context= context)
+
+def contactview(request):
+    template = 'mainapp/contact.html'
+    context = {}
+
+    return render(request, template_name=template , context=context)
+    
